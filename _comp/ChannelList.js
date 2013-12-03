5ec3bdd4df0f27b93fd679a41f9a83141b433f64
Sean <slevy@mediasignage.com>
2013-12-02 20:57:29 -0800

    applied changes to theme and applied GNU GENERAL PUBLIC LICENSE V3

--- _comp/ChannelList.js
+++ _comp/ChannelList.js
@@ -202,7 +202,7 @@ ChannelList.prototype = {
             $('.selectedResource').css({
                 'background-image': 'none',
                 'border-top': 'none',
-                'border-bottom': '1px solid #e0e0e0'
+                'border-bottom': '1px solid #e2e2e2'
             });
 
             $(resourceElem).css({

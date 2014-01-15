1f0f8048cd046d3b64dc871952f79656767d48b6
Sean <slevy@mediasignage.com>
2014-01-15 12:13:07 -0800

    testing transition / sides

--- _views/StackView.js
+++ _views/StackView.js
@@ -16,14 +16,28 @@
         factory.call(window, Backbone, window._);
     }
 }(typeof global === "object" ? global : this, function (Backbone, _) {
-    
+
     var StackView = Backbone.StackView = {};
 
     StackView.ViewPort = Backbone.View.extend({
 
-        initialize: function(){
+        initialize: function () {
+
+            var self = this;
             this.m_counter = 0;
-            this.Config = {};
+
+            this.homePage = document.getElementById("homePage");
+            this.page1 = document.getElementById("p1");
+            this.currentPage = this.homePage;
+
+            $('#b1').click(function(){
+                self.slidePageFrom(self.page1, 'right')
+            });
+
+            $('#b2').click(function(){
+                self.slidePageFrom(self.homePage, 'left')
+            });
+
         },
 
         getView: function () {
@@ -45,7 +59,7 @@
             this.m_counter++;
             $(i_view.el).attr('data-viewstackname', 'tab' + this.m_counter);
             var t = -1;
-            $('#'+this.el.id + '> *').each(function () {
+            $('#' + this.el.id + '> *').each(function () {
                 t++;
                 if (this === elem[0]) {
                     return false;
@@ -64,12 +78,22 @@
 
             var self = this.self;
 
-            $('#'+this.el.id + '> *').each(function (i) {
+            $('#' + this.el.id + '> *').each(function (i) {
                 if (i_index == i) {
                     // commBroker.fire(self.VIEW_CHANGED, this, self, i_index);
                     $(this).siblings().hide().end().fadeIn();
                 }
             });
+        },
+
+        slidePageFrom: function (page, from) {
+            var self = this;
+            // Position the page at the starting position of the animation
+            page.className = "page " + from;
+            // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
+            page.className = "page transition center";
+            self.currentPage.className = "page transition " + (from === "left" ? "right" : "left");
+            self.currentPage = page;
         }
 
     });

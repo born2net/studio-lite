3cdea181e7e1a324b9661baffddc2f83d9b382a9
Sean <slevy@mediasignage.com>
2014-03-05 10:19:24 -0800

    release build

--- _utils/releaseModules.sh
+++ _utils/releaseModules.sh
@@ -3,4 +3,4 @@ rm -r -f ../../_studiolite-bk/
 rm -r -f ../../_studiolite-dist/
 cp -r -f ../../_studiolite-dev/ ../../_studiolite-bk/
 r.js -o app.build.js
-php -q ./presetDist.php
+./presetDist.js

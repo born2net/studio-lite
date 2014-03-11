1f7046611034942b629be0b0797350d8c4020150
Sean <slevy@mediasignage.com>
2014-03-11 15:35:42 -0700

    fuelex support

---
+++ _common/_js/fuelux/src/loader.js
@@ -0,0 +1,15 @@
+/*
+ * Fuel UX
+ * https://github.com/ExactTarget/fuelux
+ *
+ * Copyright (c) 2012 ExactTarget
+ * Licensed under the MIT license.
+ */
+
+define('jquery', [], function () { return jQuery; });
+
+define('moment', [], function () {});
+
+define('fuelux/loader', ['fuelux/all'], function () {});
+
+require('fuelux/loader');

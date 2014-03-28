340eeae2cdce635e249a83d7bdad3359b83308a2
Sean <slevy@mediasignage.com>
2014-03-27 19:17:29 -0700

    working on dimensions component

--- _views/DimensionProps.js
+++ _views/DimensionProps.js
@@ -19,34 +19,48 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
             $('input', self.$el).closest('.spinner').spinner({value: 12, min: 10, max: 127, step: 1});
         },
 
+        /**
+         Listen to backbone events (maintain this)
+         @method events
+         **/
         events: {
             'click': '_onChangedValue',
             'click .pushToTopButton': '_onPushToTopLayer',
             'focusout input': function (e) {
                 var name = $(e.target).siblings('div').attr('name');
                 var value = +$(e.target).val();
-                this._updateDimenstion(name, value);
+                this._updateDimensions(name, value);
             }
         },
 
+        /**
+         Listen to changes in value of steppers for x y w h and angle
+         @method _onChangedValue
+         @param {event} e
+         **/
         _onChangedValue: function (e) {
             var self = this;
             var name = $(e.target).closest('div').attr('name');
             var value = $(e.target).closest('div').siblings('input').val();
-            self._updateDimenstion(name, value);
+            self._updateDimensions(name, value);
             e.preventDefault();
             return false;
         },
 
-        _updateDimenstion: function (i_name, i_value) {
+        /**
+         Update the dimensions for x y w h and angle in jalapeno
+         @method _updateDimensions
+         @param {String} i_name
+         @param {Number} i_value
+         **/
+        _updateDimensions: function (i_name, i_value) {
             if (i_name !== undefined && i_value !== undefined)
               log(i_name + ' ' + i_value);
         },
 
         _onPushToTopLayer: function (e) {
-            log('top')
+            log('push to top layer')
         }
-
     });
 
     return DimensionProps;

d737aa8ef822eb9bf19b7876ba1da8fbd9ff1049
Sean <slevy@mediasignage.com>
2014-07-29 13:56:54 -0700

    video on dist

--- _utils/presetDist.js
+++ _utils/presetDist.js
@@ -3,16 +3,15 @@
 var ms = require("mslibmod");
 var $ = ms.$;
 
-/**
- Update init page with dist
- **/
+// Update init page with dist
 var initPage = ms.fs.readFileSync('../../_studiolite-dist/init.js','utf8');
 initPage = initPage.replace(/-dev/gi, '-dist');
 ms.fs.writeFileSync('../../_studiolite-dist/init.js', initPage, 'utf8');
 
-/**
- Update studiolite with dist
- **/
+// Update studiolite with dist
 var studiolite = ms.fs.readFileSync('../../_studiolite-dist/studiolite.html','utf8');
 studiolite = studiolite.replace(/-dev/gi, '-dist');
+
+// add video links on dist
+studiolite = studiolite.replace(/<!-- <source src="StudioLite.mp4" type='video\/mp4'> -->/gi, '<source src="StudioLite.mp4" type="video/mp4">');
 ms.fs.writeFileSync('../../_studiolite-dist/studiolite.html', studiolite, 'utf8');

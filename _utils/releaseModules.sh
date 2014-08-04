4cd64f66e1a1b38ec16cca5eefe10adea7fc966f
Sean <slevy@mediasignage.com>
2014-08-04 13:28:55 -0700

    added import to english on release

--- _utils/releaseModules.sh
+++ _utils/releaseModules.sh
@@ -2,6 +2,7 @@ dos2unix *
 rm -r -f ../../_studiolite-bk/
 rm -r -f ../../_studiolite-dist/
 cp -r -f ../../_studiolite-dev/ ../../_studiolite-bk/
+/var/www/sites/dynasite/htdocs/_msportal/_js/_node/localImportEN.js
 r.js -o app.build.js
 ./presetDist.js
 tar -pczf /tmp/studiolite-dist.tar.gz ../../_studiolite-dist/

f2d3951a37e838c3b10ad8389f2f648994e759d1
Sean <slevy@mediasignage.com>
2014-08-19 21:42:26 -0700

    updated link to import keys

--- _utils/releaseModules.sh
+++ _utils/releaseModules.sh
@@ -2,7 +2,7 @@ dos2unix *
 rm -r -f ../../_studiolite-bk/
 rm -r -f ../../_studiolite-dist/
 cp -r -f ../../_studiolite-dev/ ../../_studiolite-bk/
-/var/www/sites/dynasite/htdocs/_msportal/_js/_node/localization/localImportLite_en.js
+/var/www/sites/dynasite/htdocs/_msportal/_js/_node/localization/importLite_en.js
 r.js -o app.build.js
 ./presetDist.js
 tar -pczf /tmp/studiolite-dist.tar.gz ../../_studiolite-dist/

727408e028f7ebff6078486bbe3171e123ebf395
Sean <slevy@mediasignage.com>
2014-10-16 17:47:55 -0700

    docs now use bootstrap theme

--- _utils/msdocGen.bat
+++ _utils/msdocGen.bat
@@ -2,4 +2,6 @@ cd C:\msweb\signagestudio_web-lite\_utils\
 node updVersion.js
 cd C:\msweb\signagestudio_web-lite
 start "" /b cmd /c "uploadVerFiles.bat"
-yuidoc .\
+rem yuidoc .\
+
+yuidoc .\ -t _doctheme -H _doctheme\helpers\helpers.js

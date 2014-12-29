8dc9aaf91c454d9c5b50ad842649c588f1d4a341
Sean <slevy@mediasignage.com>
2014-12-29 11:50:29 -0800

    milestone REST

--- _views/_fasterQ/FasterQCreatorView.js
+++ _views/_fasterQ/FasterQCreatorView.js
@@ -57,10 +57,10 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
 
         _populateLines: function () {
             var self = this;
-            $(Elements.FASTERQ_CUSTOMER_LINES).empty();
             self.m_linesCollection.sort();
             self.m_linesCollection.fetch({
                 success: function (data) {
+                    $(Elements.FASTERQ_CUSTOMER_LINES).empty();
                     _.each(data.models, $.proxy(self._appendNewLine, self));
                     self._listenLineSelected();
                 },
@@ -98,8 +98,6 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
         _listenRemoveLine: function(){
             var self = this;
             $(Elements.FASTERQ_REMOVE_LINE).on('click',function(){
-                //self.m_linesCollection.sync();
-                //return;
                 if (_.isUndefined(self.m_selectedLineID)){
                     alert('not selected');
                     return;
@@ -126,10 +124,9 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
          **/
         _listenInputNameChange: function () {
             var self = this;
-
-            // Text input change
             var onChange = _.debounce(function (e) {
                 var text = $(e.target).val();
+
                 if (_.isUndefined(self.m_selectedLineID))
                     return;
                 var model = self.m_linesCollection.get(self.m_selectedLineID);
@@ -137,94 +134,16 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
                 model.save({},{
                     success: function (model, response) {
                         self._populateLines();
-                        log('model deleted');
+                        log('model updated');
                     }, error: function () {
                         log('error delete failed');
                     }
                 });
 
-            }, 100);
+            }, 10);
             self.m_inputChangeHandler = $(Elements.SELECTED_LINE_NAME).on("input", onChange);
         }
     });
 
     return FasterQCreatorView;
 });
-
-
-/*
-
-
-
- var mod = Backbone.Model.extend({
- urlRoot: '/remoteValues',
- defaults: {
- name: 'Sean',
- last: 'Levy'
- }
- });
-
-
- var col = Backbone.Collection.extend({
- url: '/remoteAllValues/123',
- model: mod
- });
-
- window.m = new mod({
- address: 'janlor'
- });
-
- window.m2 = new mod({
- address: 'randy',
- id: '1234'
- });
-
- window.c1 = new col(m2);
- window.c1.add(m);
-
- window.c2 = new col();
- window.c2.fetch();
-
- c1.save();
-
- setTimeout(function () {
-
-
- window.m2.fetch({att1: "value"}, {
- success: function () {
- window.m.save();
-
- }, error: function () {
- log('saved failed');
- }
- });
-
- }, 2000);
-
- setTimeout(function () {
-
- window.m.save({att1: "value"}, {
- success: function () {
- window.m.save();
-
- }, error: function () {
- log('saved failed');
- }
- });
- }, 3000);
-
- setTimeout(function () {
-
- m.on('destroy', function (e) {
- log('model deleted 1');
- });
- m.destroy({
- success: function (model, response) {
- log('model deleted 2');
- log(response);
- }, error: function () {
- log('error delete failed');
- }
- });
- }, 6000)
- */

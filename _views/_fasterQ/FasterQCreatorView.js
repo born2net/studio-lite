28230a8a7d7173dc2294ce113ced54cc8bbc8679
Sean <slevy@mediasignage.com>
2014-12-29 13:12:57 -0800

    comments + changed the way line item is selected

--- _views/_fasterQ/FasterQCreatorView.js
+++ _views/_fasterQ/FasterQCreatorView.js
@@ -37,24 +37,26 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
         },
 
         /**
-         Listen to resource selection, populate the properties panel and open it if needed.
+         Listen to Line selection, populate the properties panel and open it if needed.
          @method _listenResourceSelected
          **/
         _listenLineSelected: function () {
             var self = this;
-
             $(Elements.CLASS_LINE_LIST_ITEMS).off('click');
             $(Elements.CLASS_LINE_LIST_ITEMS).on('click', function (e) {
                 var lineElem = $(e.target).closest('li');
                 self.m_selectedLineID = $(lineElem).data('line_id');
-                $(Elements.CLASS_LINE_LIST_ITEMS).removeClass('activated').find('a').removeClass('whiteFont');
-                $(lineElem).addClass('activated').find('a').addClass('whiteFont');
+                self._selectLine();
                 $(Elements.SELECTED_LINE_NAME).val(self.m_linesCollection.get(self.m_selectedLineID).get('name'));
                 self.m_property.viewPanel(Elements.FASTERQ_LINE_PROPERTIES);
                 return false;
             });
         },
 
+        /**
+         Popular the list Line items from server
+         @method _populateLines
+         **/
         _populateLines: function () {
             var self = this;
             self.m_linesCollection.sort();
@@ -63,6 +65,7 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
                     $(Elements.FASTERQ_CUSTOMER_LINES).empty();
                     _.each(data.models, $.proxy(self._appendNewLine, self));
                     self._listenLineSelected();
+                    self._selectLine();
                 },
                 error: function () {
                     log('error loading collection data....................');
@@ -70,11 +73,33 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
             });
         },
 
+        /**
+         Set the selected line background color properties
+         @method _selectLine
+         **/
+        _selectLine: function(){
+            var self = this;
+            if (_.isUndefined(self.m_selectedLineID))
+                return;
+            var lineElem = $(Elements.FASTERQ_CUSTOMER_LINES).find('[data-line_id="' + self.m_selectedLineID + '"]');
+            $(Elements.CLASS_LINE_LIST_ITEMS).removeClass('activated').find('a').removeClass('whiteFont');
+            $(lineElem).addClass('activated').find('a').addClass('whiteFont');
+        },
+
+        /**
+         Append the model Line item to list in UI
+         @method _appendNewLine
+         @param {i_model} Model
+         **/
         _appendNewLine: function (i_model) {
             var self = this;
             $(Elements.FASTERQ_CUSTOMER_LINES).append(self.m_fasterQLineItemTemplate(i_model.toJSON()));
         },
 
+        /**
+         Listen to new Line item button
+         @method _listenAddNewLine
+         **/
         _listenAddNewLine: function () {
             var self = this;
             $(Elements.FATSERQ_ADD_NEW_LINE).on('click', function (e) {
@@ -95,6 +120,10 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
             });
         },
 
+        /**
+         Listen to remove existing Line item button
+         @method _listenRemoveLine
+         **/
         _listenRemoveLine: function(){
             var self = this;
             $(Elements.FASTERQ_REMOVE_LINE).on('click',function(){
@@ -103,22 +132,20 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
                     return;
                 }
                 var model = self.m_linesCollection.get(self.m_selectedLineID);
-
                 model.destroy({
                     success: function (model, response) {
-                        //log('model deleted');
+                        log('model deleted');
+                        self._populateLines();
+                        self.m_selectedLineID = undefined;
                     }, error: function () {
                         log('error delete failed');
                     }
                 });
-
-                self._populateLines();
-                self.m_selectedLineID = undefined;
             });
         },
 
         /**
-         When Line name changed
+         Listen to changes in Line item rename through properties
          @method _listenInputNameChange
          @return none
          **/
@@ -140,7 +167,7 @@ define(['jquery', 'backbone', 'LinesCollection', 'LineModel', 'text!_templates/_
                     }
                 });
 
-            }, 10);
+            }, 400);
             self.m_inputChangeHandler = $(Elements.SELECTED_LINE_NAME).on("input", onChange);
         }
     });

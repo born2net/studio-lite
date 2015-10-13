f4f4a8f2ed21c6e499e482552332ec7216e2bcb5
root <born2net@gmail.com>
2015-10-13 10:37:53 -0700

    done with adding location points

---
+++ _utils/uploadDocs.bat
@@ -0,0 +1,1 @@
+rsync --progress  --chmod=ug=rwx --chmod=o=rx -av --stats "/cygdrive/c/msweb/_msdocs/" root@digitalsignage.com:/var/www/sites/mediasignage.com/htdocs/msdocs
---
+++ uploadVerFiles.bat
@@ -0,0 +1,2 @@
+scp /cygdrive/c/msweb/signagestudio_web-lite/package.json Sean@digitalsignage.com:/var/www/sites/dynasite/htdocs/_studiolite-dev/
+scp /cygdrive/c/msweb/signagestudio_web-lite/studiolite.html Sean@digitalsignage.com:/var/www/sites/dynasite/htdocs/_studiolite-dev/

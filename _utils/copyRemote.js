528e98e16c9c33411a1225915d641ab2b0501b01
root <born2net@gmail.com>
2016-07-20 06:25:01 -0700

    rename redirect to studiolite.html in server release script

--- _utils/copyRemote.js
+++ _utils/copyRemote.js
@@ -11,8 +11,10 @@ var ftps = new FTPS({
 
 var cmd1 = 'lftp -e "mirror --exclude node_modules/ --exclude assets/flags --exclude assets/scenes/original/ --exclude logs/ -R /var/www/sites/dynasite/htdocs/_studiolite-dist /" -u' + process.env["usr1"] + ',' + process.env["acc3"] + ' galaxy.signage.me';
 var cmd2 = 'lftp -e "rm studiolite.html" -u' + process.env["usr1"] + ',' + process.env["acc3"] + ' galaxy.signage.me';
+var cmd3 = 'lftp -e "mv redirect.html studiolite.html" -u' + process.env["usr1"] + ',' + process.env["acc3"] + ' galaxy.signage.me';
 ftps.raw(cmd1);
 ftps.raw(cmd2);
+ftps.raw(cmd3);
 ftps.raw('exit');
 ftps.exec(function (err, res) {
     var d = res.data.split('\n')

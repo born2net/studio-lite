09759abad1925523ac0d2f78ef07b440da8cf0b7
root <born2net@gmail.com>
2017-04-20 08:47:34 -0700

    fixed business rename

--- src/services/redpepper.service.ts
+++ src/services/redpepper.service.ts
@@ -2181,7 +2181,7 @@ export class RedPepperService {
      @param {Function} i_callBack
      **/
     changeBusinessName(i_email, i_password, i_businessName, i_callBack) {
-        var url = window.g_protocol + window.g_masterDomain + '/WebService/createNewAccount.ashx?command=ChangeBusinessName&userName=' + i_email + '&password=' + i_password + '&busnessName=' + i_businessName + '&callback=?';
+        var url = window.g_protocol + window.g_masterDomain + '/WebService/createNewAccount.ashx?command=ChangeBusinessName&userName=' + i_email + '&password=' + i_password + '&newBusnessName=' + i_businessName + '&callback=?';
         $.getJSON(url, i_callBack);
     }
 

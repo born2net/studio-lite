9f27468aaac68a6828b483cbff8701e919a33f8e
root <born2net@gmail.com>
2017-04-27 15:02:57 -0700

    milestone localization

--- src/locale/translator.js
+++ src/locale/translator.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const fsextra = require('fs-extra');
 const parseString = require('xml2js').parseString;
 const replace = require("replace");
-const readline = require('linebyline');
 const fetch = require('node-fetch');
 const spawn = require('child_process').spawn;
 var jsonEnglishLibrary = {};
@@ -19,12 +18,9 @@ var path = '';
 if (process.argv["2"] == 'debug')
     path = './src/locale/';
 
-const serverTranslation = (i_word) => {
+const serverTranslation = (i_lang, i_word) => {
     return new Promise((resolve, reject) => {
-
-        // var response = fetch(`https://secure.digitalsignage.com/getLocal/he/xxx/${text}`).then(res => res.json())
-
-        fetch(`https://secure.digitalsignage.com/getLocal/he/xxx/${i_word}`, {method: 'POST', body: 'a=1'})
+        fetch(`https://secure.digitalsignage.com/getLocal/${i_lang}/xxx/${i_word}`, {method: 'POST', body: 'a=1'})
             .then(res => res.json())
             .then(json => {
                 resolve(json.translated)
@@ -32,80 +28,65 @@ const serverTranslation = (i_word) => {
                 console.log('problem translating ' + i_word);
                 resolve('...')
         });
-
-        // fetch(`https://secure.digitalsignage.com/getLocal/he/xxx/${text}`).then(res => {
-        //     var result = res.json()
-        //     var result = res.json()
-        //     resolve(result.translated);
-        // })
-    });
-}
-
-const processLangFileOld = (i_lang) => {
-    return new Promise((resolve, reject) => {
-        const fileName = `${path}${i_lang}.xtb`;
-        console.log('injecting jsonEnglishLibrary to ' + fileName);
-        rl = readline(fileName);
-        rl.on('line', function (line, lineCount, byteCount) {
-            console.log(line);
-        });
-        rl.on('close', function () {
-            console.log('injecting jsonEnglishLibrary completed for ' + fileName);
-            resolve('');
-        })
     });
 }
 
 const processLangFile = (i_lang) => {
-    const fileName = `${path}${i_lang}.xtb`;
-    var xml = fs.readFileSync(fileName, 'utf8');
-    var newTranslations = '';
+    return new Promise((resolve, reject) => {
+        const fileName = `${path}${i_lang}.xtb`;
+        var xml = fs.readFileSync(fileName, 'utf8');
+        var newTranslations = '';
+        co(function* processLangFile() {
+            try {
+                for (var i = 0; i < jsonEnglishLibrary.messagebundle.msg.length; i++) {
+                    var item = jsonEnglishLibrary.messagebundle.msg[i];
+                    var id = item.$.id;
+                    var text = item._;
 
-    co(function* processLanguage() {
-        try {
-
-            for (var i = 0; i < jsonEnglishLibrary.messagebundle.msg.length; i++) {
-                var item = jsonEnglishLibrary.messagebundle.msg[i];
-                var id = item.$.id;
-                var text = item._;
-                if (xml.indexOf(id) == -1) {
-                    console.log(` ${i} doing google translation ${text}`);
-                    var translatedWord = yield serverTranslation(text)
-                    newTranslations = newTranslations + `\n<translation id="${id}">${translatedWord}</translation>`;
-                } else {
-                    newTranslations = newTranslations + `\n<translation id="${id}">${text}</translation>`;
+                    // did not find entry so let's translate with google
+                    if (xml.indexOf(id) == -1) {
+                        console.log(` ${i} doing google translation ${text}`);
+                        var translatedWord = yield serverTranslation(i_lang, text)
+                        newTranslations = newTranslations + `\t<translation id="${id}">${translatedWord}</translation>\n`;
+                    }
                 }
+                if (newTranslations != '') {
+                    replace({
+                        regex: "\<\/translationbundle\>",
+                        replacement: `${newTranslations}\n\<\/translationbundle\>`,
+                        paths: [fileName],
+                        recursive: false,
+                        silent: false
+                    });
+                    console.log(newTranslations);
+                }
+                resolve();
+            } catch (err) {
+                reject('error processLangFile ' + err);
+                console.log('processLangFile error 0: ', err, err.stack);
             }
-            if (newTranslations != '') {
-                console.log(newTranslations);
-            }
-
-        } catch (err) {
-            console.log('processLanguage error 0: ', err, err.stack);
-        }
-    }).then(function () {
-        // ms.log('done all');
-    }, function (err) {
-        console.log('processLanguage error 1: ', err, err.stack);
-    });
-
-
+        }).then(function () {
+            // ms.log('done all');
+        }, function (err) {
+            console.log('processLangFile error 1: ', err, err.stack);
+        });
+    })
 }
 
-const processLanguage = () => {
-    co(function* processLanguage() {
+const processLanguages = () => {
+    co(function* processLanguages() {
         try {
             for (var i = 0; i < languages.length; i++) {
                 var lang = languages[i];
-                processLangFile(lang);
+                yield processLangFile(lang);
             }
         } catch (err) {
-            console.log('processLanguage error 0: ', err, err.stack);
+            console.log('processLanguages error 0: ', err, err.stack);
         }
     }).then(function () {
         // ms.log('done all');
     }, function (err) {
-        console.log('processLanguage error 1: ', err, err.stack);
+        console.log('processLanguages error 1: ', err, err.stack);
     });
 }
 
@@ -128,6 +109,7 @@ const createNewLanguageFiles = () => {
 
 const generateSourceTranslationFile = () => {
     var genTranslateFile = spawn(cmd, ['run', 'x_translate'], {stdio: 'inherit'});
+    // var genTranslateFile = spawn(cmd, ['run', 'x_bump'], {stdio: 'inherit'}); // debug
     console.log('generating ./src/local/messages.xmb');
     genTranslateFile.on('error', (err) => {
         console.error(err);
@@ -138,7 +120,7 @@ const generateSourceTranslationFile = () => {
     });
     genTranslateFile.on('close', (std) => {
         createNewLanguageFiles();
-        processLanguage();
+        processLanguages();
     });
 }
 

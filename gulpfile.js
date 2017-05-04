b8f288d13271c563db5b47d434274a59aa084303
root <born2net@gmail.com>
2017-05-04 12:08:21 -0700

    auto update css on release / dev modes

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,26 @@ var gulp = require('gulp');
 var git = require('gulp-git');
 var runSequence = require('run-sequence');
 var bump = require('gulp-bump');
+var replace = require('gulp-replace');
 
 gulp.task('x_bump', function(){
     gulp.src('./package.json')
         .pipe(bump({type:'PATCH', indent: 4 }))
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('cssDev', function(){
+    gulp.src(['src/styles/style.css'])
+        .pipe(replace(/\/\*\*dev_mode_start\*\*\/[^]+\/\*\*dev_mode_end\*\*\//gim, cssDev))
+        .pipe(gulp.dest('src/styles/'));
+});
+
+gulp.task('cssRelease', function(){
+    gulp.src(['src/styles/style.css'])
+        .pipe(replace(/\/\*\*dev_mode_start\*\*\/[^]+\/\*\*dev_mode_end\*\*\//gim, cssRelease))
+        .pipe(gulp.dest('src/styles/'));
+});
+
 
 /** Dangerous, this will wipe your current source and sync with GitHub **/
 gulp.task('vanish***', function (done) {
@@ -35,3 +49,28 @@ gulp.task('x_gitReset', function () {
         if (err) throw err;
     });
 });
+
+
+var cssRelease = `/**dev_mode_start**/
+small.debug {
+    color: red;
+    display: none !important;
+}
+
+small.release {
+    text-transform: lowercase;
+    color: #4c4c4c;
+}
+/**dev_mode_end**/`;
+
+var cssDev = `/**dev_mode_start**/
+small.debug {
+    color: red;
+}
+
+small.release {
+    text-transform: lowercase;
+    color: #4c4c4c;
+    display: none !important;
+}
+/**dev_mode_end**/`;
